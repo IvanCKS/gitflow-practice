da55e4787bd88af90afd766a76228bbb7b3a1537
IvanStudy2 <valeev.ivnn@gmail.com>
2026-05-23 23:17:18 +0300

    #3 move greeting string to constants

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+const DEFAULT_NAME = 'Guest';
+const GREETING_PREFIX = 'Hello';
+
 function getGreeting(name) {
-  return `Hello, ${name}!`;
+  const userName = name || DEFAULT_NAME;
+
+  return `${GREETING_PREFIX}, ${userName}!`;
 }
 
 console.log(getGreeting('Ivan'));
